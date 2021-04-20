c075a8a6a2bb2eb47a5c1a9684254e48394a01b2
SergeyGolidonov <3apa3a86@inbox.ru>
2021-04-20 23:44:26 +0400

    Без ограничения по числам и без римских цифр

--- src/main/java/ru/golidonov/JavaMentorTest.java
+++ src/main/java/ru/golidonov/JavaMentorTest.java
@@ -1,7 +1,28 @@
 package main.java.ru.golidonov;
 
+import main.java.ru.golidonov.actions.ExpressionLogic;
+import main.java.ru.golidonov.actions.Symbol;
+import main.java.ru.golidonov.actions.SymbolsPool;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class JavaMentorTest {
     public static void main(String[] args) {
-        System.out.println("Start test");
+
+        System.out.println("*********************************************");
+        System.out.println("*Введите математическое выражение из 2х цифр*");
+        System.out.println("*====Пример: 3*5 или 2+2 или 8-4 или 9/3====*");
+        System.out.println("*********************************************");
+        Scanner scanner = new Scanner(System.in);
+        String result;
+        result = scanner.next();
+
+        Symbol symbol = new Symbol();
+        List<Symbol> symbols = symbol.iterSymbol(result);
+        SymbolsPool symbolsPool = new SymbolsPool(symbols);
+        ExpressionLogic expressionLogic = new ExpressionLogic();
+        System.out.println(expressionLogic.expression(symbolsPool));
+
     }
 }
---
+++ src/main/java/ru/golidonov/actions/SymbolTypes.java
@@ -0,0 +1,10 @@
+package main.java.ru.golidonov.actions;
+
+public enum SymbolTypes {
+    PLUS,
+    MINUS,
+    MULTIPLE,
+    DIVIDE,
+    NUMBER,
+    END_OF_LINE
+}
